331a385440b289ad4440afd8b9bd51ea66152c54
souvik150 <souvikmukherjee150@gmail.com>
2023-07-12 01:42:10 +0530

    feat: adds

--- src/components/home/customer.tsx
+++ src/components/home/customer.tsx
@@ -10,9 +10,9 @@ const Customer = () => {
                 <h1 className="text-primary-heading text-center font-bold">What Our Customers Say</h1>
                 <p className="text-normal-text text-justify md:text-center font-light my-4">Our customers rave about the convenience and effectiveness of our platform. They love the ability to connect with verified experts from different industries for instant advice and guidance. From bite-sized consultations to personalized mentorship, they've found success and growth through our community. Join the many satisfied customers who are raving about the positive impact we've made on their lives and careers!</p>
 
-                <div className="flex flex-col gap-10 lg:flex-row justify-center my-16 items-center">
+                <div className="flex flex-col gap-10 md:flex-row justify-center my-16 items-center">
 
-                    <div className="bg-grey_bg mx-10 flex flex-col items-center w-full lg:w-1/4 relative px-20 py-10 rounded-3xl text-center">
+                    <div className="bg-grey_bg mx-10 flex flex-col items-center w-full md:w-1/4 relative px-20 py-10 rounded-3xl text-center">
                     <Image src={ProfPhoto} alt="" className="absolute -top-6"/>
                         <p className="pt-10">
                     Nakul sir, was a great help to get clarity about my entrepreneurial journey.
@@ -21,7 +21,7 @@ const Customer = () => {
                     <h4 className="text-tertiary-heading text-yellow_text font-semibold mt-2">Taran Kumar</h4>
                     <p className="font-bold text-small-text">Student Entrepreneur</p>
                     </div>
-                    <div className="bg-grey_bg mx-10 flex flex-col items-center w-full lg:w-1/4 relative px-20 py-10 rounded-3xl text-center">
+                    <div className="bg-grey_bg mx-10 flex flex-col items-center w-full md:w-1/4 relative px-20 py-10 rounded-3xl text-center">
                         <Image src={ProfPhoto1} alt="" className="absolute -top-6"/>
                         <p className="pt-10">
                             Nakul sir, was a great help to get clarity about my entrepreneurial journey.
@@ -30,7 +30,7 @@ const Customer = () => {
                         <h4 className="text-tertiary-heading text-yellow_text font-semibold mt-2">Taran Kumar</h4>
                         <p className="font-bold text-small-text">Student Entrepreneur</p>
                     </div>
-                    <div className="bg-grey_bg mx-10 flex flex-col items-center w-full lg:w-1/4 relative px-20 py-10 rounded-3xl text-center">
+                    <div className="bg-grey_bg mx-10 flex flex-col items-center w-full md:w-1/4 relative px-20 py-10 rounded-3xl text-center">
                         <Image src={ProfPhoto2} alt="" className="absolute -top-6"/>
                         <p className="pt-10">
                             Nakul sir, was a great help to get clarity about my entrepreneurial journey.
